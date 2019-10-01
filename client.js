6cae75d72461d5d475b9e1e7110c9a3e94c32f25
JohnReiland <reiland.shanghai@gmail.com>
2019-09-30 23:56:53 -0400

    "Add full username selection functionality"

--- client.js
+++ client.js
@@ -1,13 +1,20 @@
-const form = document.querySelector('form');
-const tweedsElement = document.querySelector('.tweeds');
 streams.users.visitor = [];
 const visitor = "visitor";
+var focus = undefined;
+const form = document.querySelector('form');
+const tweedsElement = document.querySelector('.tweeds');
+
+function changeFocusTo(user) {
+  focus = user;
+  console.log('focus changed to ', focus);
+}
+
 
 form.addEventListener('submit', (event) => {
   event.preventDefault();
   var formData = new FormData(form);
   var message = formData.get('content');
-  console.log(visitor, message);
+  console.log(visitor, message, focus);
   if (message !== '') {
     writeTweet(message);
   }
